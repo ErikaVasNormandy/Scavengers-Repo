a26787bb26ee4f2805588044ce9a093e82b76d89
server <erika.h.2012@gmail.com>
2017-01-28 06:12:06 +0000

    hi

---
+++ app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask
+from flask import request, redirect
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	Myauthor = "Me"
+	Myname = "You"
+	return render_template("index.html", author=Myauthor, name=Myname )
+
+@app.route('/profile')
+def profilePage():
+    return render_template("profile.html")
+
+@app.route('/formdemo')
+def formDemo():
+    return render_template("formDemo.html")
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+    email = request.form['email']
+    print("the email address is '" + email + "'")
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
