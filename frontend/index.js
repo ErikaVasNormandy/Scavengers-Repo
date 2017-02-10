43448a279e7d348e1d0d960d72331c63ae3e54bb
ASCI-Red <erika.h.2012@gmail.com>
2017-02-10 15:49:58 -0800

    Base website is done, kind of

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,17 @@
 var appName = angular.module("AppForHTML", []);
 
 
+// appName.controller("backgroundController", function($scope){
+//   $scope.bgImg= {
+//     "background-color": "coral"
+//   }
+// });
+
 appName.controller("ctrl1", function($scope) {
-  $scope.profile="In ctrl1"
+  $scope.profile="A namespace for wandering minds"
+
+
+
 })
 
 appName.controller("TabController", function($scope) {
@@ -19,19 +28,6 @@ appName.controller("TabController", function($scope) {
   $scope.homeContent="Hello, my name is Erika. I am a web+mobile develoepr with an interest in cybersecurity. I built this website in order to try out Amazon's AWS EC2 tool";
   $scope.projects ="projects over here";
   $scope.contact="contact info";
-
-
-});
-appName.controller("TabController2", function($scope) {
-
-});
-
-
-appName.controller("Content", function($scope) {
-  $scope.homeContent="Hello, my name is Erika. I am a web+mobile develoepr with an interest in cybersecurity. I built this website in order to try out Amazon's AWS EC2 tool";
-  $scope.projects ="projects over here";
-  $scope.contact="contact info";
-
 
 
 });
