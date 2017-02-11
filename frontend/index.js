fd359cb4052c6793db28f1860757c69bfbac1d9e
ASCI-Red <erika.h.2012@gmail.com>
2017-02-10 16:13:45 -0800

    figured out some things with ng-style

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,15 @@
 var appName = angular.module("AppForHTML", []);
 
 
-// appName.controller("backgroundController", function($scope){
-//   $scope.bgImg= {
-//     "background-color": "coral"
-//   }
-// });
+appName.controller("backgroundController", function($scope){
+  $scope.bgImages = [" https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/960.jpg", "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/Over-the-Garden-Wall-S01E02-720p-HDTVSeriesdl.com_00_02_54_00000.png"];
+  $scope.getImage==function(num){
+    return bgImages[num];
+  }
+  $scope.bgImg= {
+    "background-image": 'url('+ $scope.bgImages[0] +')'
+  }
+});
 
 appName.controller("ctrl1", function($scope) {
   $scope.profile="A namespace for wandering minds"
