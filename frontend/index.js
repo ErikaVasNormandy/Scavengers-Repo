8f35a4a71315e8f1f24dccdae36e1230cf242b31
ASCI-Red <erika.h.2012@gmail.com>
2017-02-10 16:24:05 -0800

    figured out some things with ng-style

--- frontend/index.js
+++ frontend/index.js
@@ -2,12 +2,15 @@ var appName = angular.module("AppForHTML", []);
 
 
 appName.controller("backgroundController", function($scope){
-  $scope.bgImages = [" https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/960.jpg", "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/Over-the-Garden-Wall-S01E02-720p-HDTVSeriesdl.com_00_02_54_00000.png"];
+  $scope.bgImages = [" https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/960.jpg", "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/Over-the-Garden-Wall-S01E02-720p-HDTVSeriesdl.com_00_02_54_00000.png", "https://images2.alphacoders.com/735/thumb-1920-735771.png", "https://images.alphacoders.com/650/thumb-350-650252.jpg"];
   $scope.getImage==function(num){
     return bgImages[num];
   }
+
+  var rand = Math.floor(Math.random() * 4) + 1;
+
   $scope.bgImg= {
-    "background-image": 'url('+ $scope.bgImages[0] +')'
+    "background-image": 'url('+ $scope.bgImages[rand] +')'
   }
 });
 
