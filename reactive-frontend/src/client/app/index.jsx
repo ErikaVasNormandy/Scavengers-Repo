10daa0e7d5cb373bf5695081381e151174f981c9
Erika <erika.h.2012@gmail.com>
2017-06-28 04:01:29 +0000

    Made some progress with incorporating the css

--- reactive-frontend/src/client/app/index.jsx
+++ reactive-frontend/src/client/app/index.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import {render} from 'react-dom';
+import styles from '../../../../angular-frontend/styles.css';
 
 class App extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {currentTab: 0, tab0Active: true, tab1Active: false, tab2Active: false}
+        this.setTab = this.setTab.bind(this)
+        
     }
     
+    setTab(newTab){
+        this.state.currentTab = newTab;
+    }
+    
+    
     render() {
-        return(<div>Hello</div>)
+        return(<div>
+        
+        <ul className = "MainMenu">
+              
+                <li >
+                   <button  onClick = {() => this.setTab(0)}>Comprehensive</button>
+                </li>
+                <li >
+                    <button  onClick = {() => this.setTab(1)} >Groceries</button>
+                </li>
+                
+                <li >
+                    <button  onClick = {() => this.setTab(2)}>Utilities</button>
+                </li>
+                
+                
+              </ul>
+              
+              
+              
+              </div>)
     }
     
 }
