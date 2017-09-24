70601e455de02b40a5b52bf24f278835b70c528a
ASCI-Red <erika.h.2012@gmail.com>
2017-09-23 17:33:18 -0700

    added content div

--- reactive-frontend/src/client/app/index.jsx
+++ reactive-frontend/src/client/app/index.jsx
@@ -13,12 +13,45 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {currentTab: 0, tab0Active: true, tab1Active: false, tab2Active: false, tab3Active: false}
-        this.setTab = this.setTab.bind(this)
-
+        this.setTab = this.setTab.bind(this);
     }
 
-    setTab(newTab){
+    setTab(newTab)
+    {
         this.state.currentTab = newTab;
+        if(this.state.currentTab == 0)
+        {
+          console.log("Clicked on 0")
+          this.setState({
+            tab0Active: true,
+            tab1Active: false,
+            tab2Active: false
+          })
+
+        }
+
+        if(this.state.currentTab == 1)
+        {
+            console.log("Clicked on 1")
+          this.setState({
+              tab0Active: false,
+              tab1Active: true,
+              tab2Active: false
+            })
+        }
+        if(this.state.currentTab == 2)
+        {
+          console.log("Clicked on 2")
+          this.setState({
+              tab0Active: false,
+              tab1Active: false,
+              tab2Active: true
+            })
+          }
+
+
+        this.state.currentContent = newTab;
+        console.log(this.state.currentTab)
     }
 
 
@@ -29,16 +62,16 @@ class App extends React.Component{
               <div className="col s2 m2 l2">
                 <div className="sideMenu">
                   <ul>
-                    <li >
-                      <button  onClick = {() => this.setTab(0)} className="btn waves-effect waves-light transparent flat">Home</button>
+                    <li>
+                      <button className="transparent btn-flat" onClick = {() => this.setTab(0)}>Comprehensive</button>
                     </li>
 
                     <li >
-                      <button  onClick = {() => this.setTab(1)}className="btn waves-effect waves-light transparent flat">About</button>
+                      <button className=" transparent btn-flat" onClick = {() => this.setTab(1)} >Groceries</button>
                     </li>
 
                     <li >
-                      <button  onClick = {() => this.setTab(2)}className="btn waves-effect waves-light transparent flat">Resources</button>
+                      <button className="transparent btn-flat" onClick = {() => this.setTab(2)}>Utilities</button>
                     </li>
                   </ul>
 
@@ -47,7 +80,15 @@ class App extends React.Component{
 
               </div>
               <div className="col s9 m9 l9" id="contentContainer" >
-              <MaterialComponent id ="homeContent">hello have i rendered it yet?</MaterialComponent>
+                <MaterialComponent id ="homeContent" className= {this.state.tab0Active ? '' : 'dormantTab'} > Home Content </MaterialComponent>
+                <MaterialComponent id ="Content1" className= {this.state.tab1Active ? '' : 'dormantTab'} > Tab 1 </MaterialComponent>
+                <MaterialComponent id ="Content2" className= {this.state.tab2Active ? '' : 'dormantTab'}>Tab 2</MaterialComponent>
+
+                  // <div id = "0" className = {this.state.tab0Active ? '' : 'dormantTab'}>
+                  //                Comprehensive
+                  // </div>
+                  // <div id = "1" className = {this.state.tab1Active ? '' : 'dormantTab'}>Groceries</div>
+                  // <div id = "2" className = {this.state.tab2Active ? '' : 'dormantTab'}>Utilities</div>
 
               </div>
 
