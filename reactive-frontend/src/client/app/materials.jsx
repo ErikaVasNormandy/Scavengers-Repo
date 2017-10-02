58317fdd3d6171d6254b215d69b83b12e90a0a74
ASCI-Red <erika.h.2012@gmail.com>
2017-10-02 16:03:53 -0700

    uploading from local machine

--- reactive-frontend/src/client/app/materials.jsx
+++ reactive-frontend/src/client/app/materials.jsx
@@ -11,6 +11,8 @@ class materialComponent extends React.Component {
       <div>
         <h2>Default Content Title</h2>
         https://stackoverflow.com/questions/46360567/error-npm-is-known-not-to-run-on-node-js-v4-2-6
+
+
       </div>
     )
   }
