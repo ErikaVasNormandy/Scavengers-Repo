9d64db8586a783933702e36c8c50588c183688ec
ASCI-Red <erika.h.2012@gmail.com>
2017-10-09 18:47:12 -0700

    adding more stuff to Projects Component

--- reactive-frontend/src/client/app/components/ProjectComponent.jsx
+++ reactive-frontend/src/client/app/components/ProjectComponent.jsx
@@ -15,28 +15,56 @@ class ProjectComponent extends React.Component {
         <h1>Projects</h1>
         <p>Mostly Github links for now, planning on categorizing them more </p>
         https://github.com/ErikaVasNormandy
-        <div class="row">
-           <div class="col s12 m7">
-        <div className="card">
-          <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" style={{height: 80, width: 80}} src="https://i.pinimg.com/736x/8c/a7/21/8ca721aa046919d68bfa3187f548c539--spell-books-open-book.jpg"></img>
+         <div className="row" style = {{color: 'black'}}>
+
+
+            <div className="col m4">
+               <div className="card sticky-action">
+                    <div className="card-image">
+                        <img className="activator" src="https://i.pinimg.com/736x/8c/a7/21/8ca721aa046919d68bfa3187f548c539--spell-books-open-book.jpg"></img>
+                            <span className="card-title" style={{width:100}}>Sample1</span>
+                    </div>
+                    <div className="card-content">
+                               <p>A collection of pages from my Cybersecurity notes over the years</p>
+                    </div>
+                    <div className="card-action">
+                          <p>card action</p>
+                    </div>
+                      <div className="card-reveal"><p></p></div>
+                </div>
             </div>
-    <div className="card-content">
-      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
-    </div>
-    <div className="card-reveal">
-      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+
+            <div className="col m4">
+               <div className="card sticky-action">
+                      <div className="card-image ">
+                        <img className="activator"src="http://www.ilikewallpaper.net/ipad-wallpapers/download/2268/Square-Pattern-ipad-wallpaper-ilikewallpaper_com.jpg"></img>
+                            <span className="card-title" style={{width:100}}>Sample2</span>
+                      </div>
+                      <div className="card-content">
+                          <p></p>
+                      </div>
+                      <div className="card-action">
+                          <a href="#">This is a link</a>
+                      </div>
+                        <div className="card-reveal">...</div>
+              </div>
+            </div>
+
+
+
         </div>
-      </div>
-    </div>
-  </div>
+
+
+
+
         <ul>
 
           <li>"https://github.com/ErikaVasNormandy/Scavengers-Repo"</li>
           <li>"https://github.com/ErikaVasNormandy/DeAnza-Ethical-Hacking-Summer2016"</li>
         </ul>
+
+
+
 
       </div>
     )
