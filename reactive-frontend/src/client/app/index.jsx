def8e8031006fba0d79533d530d13ae14b06b7b8
ASCI-Red <erika.h.2012@gmail.com>
2017-12-17 00:20:52 -0800

    got transitions

--- reactive-frontend/src/client/app/index.jsx
+++ reactive-frontend/src/client/app/index.jsx
@@ -8,16 +8,52 @@ import WorldBuildingComponent from './components/WorldBuildingComponent.jsx';
 import data from './data/usefulResourcesData.js';
 import styles from '../app/styles.css';
 
-var sectionStyle= {
-  backgroundImage: "url(https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/jwHdoTn.png)"
-}
+var imgUrl="";
 
 class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {currentTab: 1, tab1Active: true, tab2Active: false, tab3Active: false, tab4Active: false}
         this.setTab = this.setTab.bind(this);
+        this.randomImage=this.randomImage.bind(this);
+
+
     }
+    getInitialState(){
+      return {imgUrl: ""};
+    }
+    componentDidMount(){
+      let timer = setInterval(this.tick, 1000);
+   this.setState({timer});
+      // setTimeout(function(){ this.setState({imgUrl :  "https://images2.alphacoders.com/735/thumb-1920-735771.png"}); }.bind(this), 5000  );
+    }
+
+    randomImage(){
+      var fileNames = [
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/SpringMoodBoard/Snapdragon-Color-Meaning.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/SpringMoodBoard/maxresdefault.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/SummerMoodBoard/thumb-1920-625703.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/SummerMoodBoard/M2T6aUy.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/SummerMoodBoard/a5432863572ed79c0972569d213dc149.png",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/SummerMoodBoard/thumb-1920-735771.png",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/SummerMoodBoard/tumblr_mvt4dfUVx71smn4pqo9_1280.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/2030_over_the_garden_wall.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/87cbb3248a40f91479e8ab7da60cb09e.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/jwHdoTn.png",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/Over-the-Garden-Wall-S01E02-720p-HDTVSeriesdl.com_00_02_54_00000.png",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/960.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/AutumnMoodBoard/efcb4c0e828c1809e38b9cdc02aea227.png",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/WinterMoodBoard/Dx5LJoH.jpg",
+        "https://s3-us-west-1.amazonaws.com/scavengers-repo.com/images/WinterMoodBoard/gHQr2Tr.jpg"
+      ];
+      var randomIndex = 4;
+      document.getElementById("smallParallax1").backgroundImage= fileNames[randomIndex];
+      setTimeout(function(){
+        randomImage();
+      }, 10000);
+
+      }
+
 
     setTab(newTab)
     {
@@ -76,6 +112,11 @@ class App extends React.Component{
 
     render() {
 
+
+      var parallax1Style = {
+        backgroundImage: 'url(' + imgUrl + ')'
+      }
+
         return(
           <div>
 
@@ -94,6 +135,7 @@ class App extends React.Component{
             <div id ="parallax">
               <div id= "smallParallax1"></div>
               <div id ="smallParallax2"></div>
+
 
 
             </div>
