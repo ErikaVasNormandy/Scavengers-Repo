bf54b2e66031130cf9801ccc5f0a2c623ea78cc2
Erika Hudiono <erikavasnormandy@gmail.com>
2018-07-18 23:44:27 -0700

    Made a first post

--- reactive-frontend/src/components/MainPageComponent.jsx
+++ reactive-frontend/src/components/MainPageComponent.jsx
@@ -12,7 +12,7 @@ class MainPageComponent extends React.Component {
         {/*load up posts, versus projects*/}
         <h3>Today</h3>
         {/* Default Post*/}
-        <DefaultPostComponent imageinput = "https://avatars0.githubusercontent.com/u/8341739?s=460&v=4" content = "A new job, and a revamped site. Now that I'm not on the job hunt, I figured it was the time to rebuild the site instead of a slapped together project. This is the place where I'll be putting all of my stuff, which can range from technomancer materials, art, geek stuff, or" />
+        <DefaultPostComponent imageinput = "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/30441830_10211585837750757_7216752123478802432_n.jpg?_nc_cat=0&oh=84b34d34a13954adc80c37faef1f8cfa&oe=5BDC269A" content = {["A new job, and a revamped site. Now that I'm not on the job hunt, I figured it was the time to rebuild the site instead of a slapped together project.",<p>This is my bun Trixie, you can't see her well but she's my player 2 when she isn't causing trouble</p>] } />
         <DefaultPostComponent/>
       </div>
     )
