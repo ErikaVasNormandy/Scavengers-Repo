325fb30c49bb83fe28ef2d1d5db235095549594d
Erika Hudiono <erikavasnormandy@gmail.com>
2018-08-29 00:15:00 -0700

    added social media

--- reactive-frontend/src/components/Posts/DefaultPostComponent.jsx
+++ reactive-frontend/src/components/Posts/DefaultPostComponent.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
 import {render} from 'react-dom';
 import InstagramEmbed from 'react-instagram-embed'
+
+import {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  RedditShareButton,
+  TumblrShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  ViberShareButton,
+  WorkplaceShareButton,
+  EmailShareButton,
+} from 'react-share';
+
+import {
+  FacebookIcon,
+  TwitterIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  GooglePlusIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  VKIcon,
+  OKIcon,
+  RedditIcon,
+  TumblrIcon,
+  LivejournalIcon,
+  MailruIcon,
+  ViberIcon,
+  WorkplaceIcon,
+  EmailIcon,
+} from 'react-share';
+
+
+
 class DefaultPostComponent extends React.Component {
   constructor(props) {
     super(props);
     this.returnDate = this.returnDate.bind(this);
   }
+
+
+
+
   returnDate(){
     var monthNames = [
       "January", "February", "March",
@@ -25,7 +70,7 @@ class DefaultPostComponent extends React.Component {
 
   render(){
     return(
-      <div className="DefaultPostComponent">
+      <div className="DefaultPostComponent" goTo={this.scrollToContent}>
         {/*load up posts, versus projects*/}
         { /*{this.returnDate()}*/}
           <h4 className="postHeader">{this.props.header}</h4>
@@ -42,10 +87,22 @@ class DefaultPostComponent extends React.Component {
               onFailure={() => {}}
               />
           <img src = {this.props.imageinput}></img>
-            <div className="postText">
+            <div className="postText" >
               {this.props.content}
             </div>
+            <div className="socialMedia">
+              <FacebookShareButton role = "button" children = {<FacebookIcon size={32} round={true} />} url = "http://scavengers-repo.com/" />
+            </div>
 
+            <div className="socialMedia">
+              <LinkedinShareButton role = "button" children = {<LinkedinIcon size={32} round={true} />} url = "http://scavengers-repo.com/" />
+            </div>
+
+
+            <div className="socialMedia">
+
+                <TumblrShareButton role = "button" children = {<TumblrIcon size={32} round={true} />} url = "http://scavengers-repo.com/" />
+            </div>
 
       </div>
     )
