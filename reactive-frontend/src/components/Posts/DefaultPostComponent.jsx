74d892fd1073d0e6829a917b8c47dac4c158df9d
Erika Hudiono <erikavasnormandy@gmail.com>
2018-08-30 00:15:40 -0700

    Anchor tags

--- reactive-frontend/src/components/Posts/DefaultPostComponent.jsx
+++ reactive-frontend/src/components/Posts/DefaultPostComponent.jsx
@@ -72,11 +72,11 @@ class DefaultPostComponent extends React.Component {
   render(){
     return(
       <div className="DefaultPostComponent" id={this.props.postID}>
-        <meta itemprop="thumbnailUrl" content="https://6d4be195623157e28848-7697ece4918e0a73861de0eb37d08968.ssl.cf1.rackcdn.com/71481_200w.jpg"/>
-        <meta name="twitter:image" content="https://6d4be195623157e28848-7697ece4918e0a73861de0eb37d08968.ssl.cf1.rackcdn.com/71481_200w.jpg"/>
-
+        <head>
+          <meta itemprop="thumbnailUrl" content="https://6d4be195623157e28848-7697ece4918e0a73861de0eb37d08968.ssl.cf1.rackcdn.com/71481_200w.jpg"/>
+          <meta name="twitter:image" content="https://6d4be195623157e28848-7697ece4918e0a73861de0eb37d08968.ssl.cf1.rackcdn.com/71481_200w.jpg"/>
+        </head>
         <MetaTags>
-          <title>Page 1</title>
           <meta name="description" content={this.props.content}/>
           <meta property="og:title" content="MyApp" />
           <meta property="og:image" content="https://6d4be195623157e28848-7697ece4918e0a73861de0eb37d08968.ssl.cf1.rackcdn.com/71481_200w.jpg" />
